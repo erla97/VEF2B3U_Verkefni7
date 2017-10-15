ce596279d9b8ab23cc86b334df4aab7ff32fd12e
erla97 <erlaos207@nemi.tskoli.is>
2017-10-15 23:35:24 +0000

    Finished

---
+++ script.js
@@ -0,0 +1,15 @@
+var button1 = document.getElementById("b1");
+var button2 = document.getElementById("b2");
+var button3 = document.getElementById("b3");
+
+button1.addEventListener("click", function(event) {
+	button1.style.backgroundColor = "red";
+});
+
+button2.addEventListener("click", function(event) {
+	button2.style.backgroundColor = "red";
+});
+
+button3.addEventListener("click", function(event) {
+	button3.style.backgroundColor = "red";
+});
